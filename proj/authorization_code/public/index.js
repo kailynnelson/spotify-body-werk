aaf99716b160b74c8bfa72d3807e9ba752756ceb
Kailyn Nelson <nelsonkailynr@gmail.com>
2021-02-28 12:07:51 -0800

    add 'Track' class, start replace function

--- proj/authorization_code/public/index.js
+++ proj/authorization_code/public/index.js
@@ -80,55 +80,103 @@
 		// 	false
 		// );
 
-		// get a list of tracks, with their playlist ID
-		var playlistTracks = [];
-		var playlistDanceabilityScores = [];
+		// TODO: allow user to input any playlist ID
+		var playlistId = '3PoDunH7BeHQxWmRhDCOfC'; // my 'body werk set u free' playlist
+
+		// a basic object returned by the GET playlist function
+		var playlist = null;
 
 		// prettier-ignore
 		document.getElementById('get-playlist').addEventListener('click', function () {
-				// give it the id of my 'body werk set u free' playlist
-				var playlist_id = '3PoDunH7BeHQxWmRhDCOfC';
 				// declare oath token: https://developer.spotify.com/console/get-playlist/
 				$.ajax({
-					url: 'https://api.spotify.com/v1/playlists/' + playlist_id,
+					url: 'https://api.spotify.com/v1/playlists/' + playlistId,
 					headers: {
 						Authorization: 'Bearer ' + access_token,
 					},
 					json: true,
 				}).done(function (data) {
 					console.log('got playlist ╰(*°▽°*)╯ ', data);
-
-					playlistTracks = data.tracks.items;
-					// console.log('got playlist tracks? ', playlist_tracks);
-
-					// TODO: this shouldn't be triggered with a button called 'Get playlist'; should add a button or clarify button text for interaction effect
-					// prettier-ignore
-					playlistDanceabilityScores = getPlaylistTrackDanceabilityScores(playlistTracks);
+					playlist = data;
 
 					playlistPlaceholder.innerHTML = playlistTemplate({
-						playlist_id: playlist_id,
-						playlist_name: data.name,
+						playlist_id: playlistId,
+						playlist_name: playlist.name,
 					});
 				});
 			});
 	}
 
+	// get a list of tracks from a given playlist ID
+	var playlistTracks = [];
+
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+	class Track {
+		constructor(originalIndex, id, uri, name) {
+			this.originalIndex = originalIndex;
+			this.id = id;
+			this.uri = uri;
+			this.name = name;
+		}
+		danceabilityScore = null;
+		newIndex = null;
+	}
+
+	// new list of playlist tracks with my custom Track objects
+	var myPlaylistTracks = [];
+
+	// create 'Track' objects (with my 'Track' class)
+	// then fill a 'playlist' array with my 'Track' objects
+	// prettier-ignore
+	document.getElementById('create-tracks').addEventListener('click', function() {
+
+		playlistTracks = playlist.tracks.items;
+		// console.log('got playlistTracks: ', playlistTracks);
+
+		for (i = 0; i < playlistTracks.length; i++) {
+			let item = playlistTracks[i];
+
+			// create a 'Track' object from my custom class 
+			let myTrack = new Track(i, item.track.id, item.track.uri, item.track.name);
+			// console.log('created myTrack: ', myTrack.name);
+
+			// push the 'Track' object to my 'myPlaylistTracks' array 
+			myPlaylistTracks.push(myTrack);
+		}
+		console.log('updated myPlaylistTracks: ', myPlaylistTracks);
+	})
+
+	// TODO: add function for getting dance scores
+	var playlistDanceabilityScores = [];
+
+	// prettier-ignore
+	document.getElementById('get-danceability-scores').addEventListener('click', function() {
+		// TODO: add error messages if the user clicks the buttons out of order 
+		// TODO: or, even better: disable the buttons until the info is available to make the interaction successful! 
+		playlistDanceabilityScores = getPlaylistTrackDanceabilityScores(playlistTracks);
+	})
+
 	// get a list of playlist tracks, with their track IDs' danceability scores
+	// declare oath token: https://developer.spotify.com/console/get-audio-features-several-tracks/
+	// tell it which audio features to get...
+	// and then once it gets the audio features, to add just the danceability score
+	// of that one track to an array of playlist-track-danceability socres
 	function getPlaylistTrackDanceabilityScores(tracks) {
 		// console.log(
 		// 	'got tracks! starting dance score method... (´▽`ʃ♡ƪ) ',
 		// 	tracks
 		// );
 
-		var trackIds = getPlaylistTracksIds(tracks);
+		// TODO: fill myPlaylistTracks' danceability scores with getPlaylistTrackDanceabilityScores
+
+		let trackIds = tracks.map((tracks) => tracks.track.id);
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+		trackIds = trackIds.join(',');
+
+		console.log('got trackIds: ', trackIds);
 		// console.log("got tracks' IDs (｡･∀･)ﾉﾞ ", trackIds);
+
 		// https://api.jquery.com/jquery.ajax/
-		// declare oath token: https://developer.spotify.com/console/get-audio-features-several-tracks/
-
-		// tell it which audio features to get...
-		// and then once it gets the audio features, to add just the danceability score
-		// of that one track to an array of playlist-track-danceability socres
-
 		$.ajax({
 			// max 100 IDs
 			url: 'https://api.spotify.com/v1/audio-features?ids=' + trackIds,
@@ -142,7 +190,6 @@
 			playlistDanceabilityScores = data.audio_features.map(
 				(data) => data.danceability
 			);
-			// TODO: !important: keep track IDs with track danceability scores... oops
 			// prettier-ignore
 			// console.log("got tracks' danceability scores! (✿◡‿◡)", playlistDanceabilityScores);
 		});
@@ -150,20 +197,11 @@
 		return;
 	}
 
-	function getPlaylistTracksIds(tracks) {
-		// https://stackoverflow.com/a/46694321/5996491
-		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-		// QUESTION: what's the difference between 'let' and 'var'?
-		let trackIds = tracks.map((tracks) => tracks.track.id);
-		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
-		trackIds = trackIds.join(',');
-		return trackIds;
-	}
-
 	// get danceability sine
 	// prettier-ignore
 	document.getElementById('get-danceability-sine').addEventListener('click', 
 		function () {
+			// give '2' for number of arcs in the ideal sine wave
 			getDanceabilitySine(playlistDanceabilityScores, 2);
 		}
 	);
@@ -189,6 +227,8 @@
 		let danceabilitySine = generateNewPlaylistByIdealSine(idealSine,playlistDanceabilityScores);
 		// prettier-ignore
 		console.log('got danceability scores by sine wave-best fit ~\(≧▽≦)/~ ', danceabilitySine);
+
+		// TODO: realized some tracks came back with 'undefined' after making some changes to the playlist; should diagnose + repair
 
 		// TODO: plot points on a graph? create a new playlist with the reorganized songs? the world is our oyster
 		// return [steps, idealSine, danceabilitySine];
@@ -285,4 +325,63 @@
 		let numRounded = Math.round(num + 'e' + places);
 		return Number(numRounded + 'e' + -places);
 	}
+
+	// get reordered playlist, based on sine wave-best fit
+	// prettier-ignore
+	document.getElementById('get-replaced-playlist').addEventListener('click', 
+		function () {
+			reorderPlaylistBySineWaveBestFit(playlistDanceabilityScores, 2);
+		}
+	);
+
+	// REPLACE (not REORDER, because we're giving URIs) given playlist with sine wave-best fit-ordered tracks
+	// https://developer.spotify.com/documentation/web-api/reference/#endpoint-reorder-or-replace-playlists-tracks
+	function reorderPlaylistBySineWaveBestFit(orderedScores) {
+		let trackUrisCsv = trackUrisToCsv(orderedScores); // track URIs as a CSV list
+		console.log('ready to rumble with the REPLACE situation...');
+
+		// $.ajax({
+		// 	// max 100 IDs
+		// 	// TODO: if more than 100 given, loop through/recall?
+		// 	url: 'https://api.spotify.com/v1/playlists/' + playlistId + '/tracks',
+		// 	headers: {
+		// 		Authorization: 'Bearer ' + access_token,
+		// 	},
+		// 	// https://stackoverflow.com/a/49598763/5996491
+		// 	type: 'PUT',
+		// 	contentType: 'application/json',
+		// 	request: {
+		// 		// QUESTION: do I want to track the snapshot_id?
+		// 		uris: trackUrisCsv,
+		// 		range_start: 0,
+		// 		insert_before: orderedScores.length,
+		// 		range_length: orderedScores.length,
+		// 	},
+		// 	json: true,
+		// })
+		// 	.done(function () {
+		// 		// TODO: enable user-person to view the songs in the reordered playlist in the app (w/o having to open spotify)
+		// 		console.log('done reordering! check spootify.');
+		// 	})
+		// 	.fail(function (message) {
+		// 		console.log(
+		// 			"oops, that didn't work :/ you tried, though. here's the error: ",
+		// 			message
+		// 		);
+		// 	});
+		return;
+	}
+
+	function trackUrisToCsv(tracks) {
+		let tracksCsv = [];
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+		for (Track in tracks) {
+			console.log('STARTING trackUrisToCsv, whoopee! ...');
+			// TODO: compile track URIs to CSV
+		}
+		return tracksCsv;
+	}
+
+	// TODO: now we're gettin' crazy. WHAT IF we auto-DJ'd tracks, giving specific start/end times and blending them? would be tough, but SICK
+	// TODO: ...what if I added sensor data, like from an Apple Watch? could get biometrics like heart beat and intentionally hype / calm user-people
 })();
